efbfd2e952dfff21bebeefee6f98de12a7c19772
Kingsley Codes <156113676+kingsleycodes247@users.noreply.github.com>
2024-02-09 12:07:31 +0000

    Add files via upload

---
+++ main_file.py
@@ -0,0 +1,11 @@
+from tkinter import *
+from login import Login
+from billclass import BillClass
+from dashboard import IMS
+
+
+
+if __name__ == "__main__":
+    root = Tk()
+    app = Login(root)
+    root.mainloop()
